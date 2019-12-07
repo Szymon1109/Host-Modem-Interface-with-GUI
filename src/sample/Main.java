d12d35a4afc0dc3c0a089b4d620cc6c2b9c2609e
Szymon1109 <szymonbetlewski@wp.pl>
2019-12-07 21:59:08 +0100

    Changed window size

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
         primaryStage.setTitle("Communication Interface");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 650, 400));
         primaryStage.show();
     }
 
